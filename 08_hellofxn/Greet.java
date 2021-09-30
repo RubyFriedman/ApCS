197cb01b5145c9f53d69a3e6c0fa9a65a4de5e7a
rubyfriedman <rfriedman30@stuy.edu>
2021-09-30 19:53:16 -0400

    init setup

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -7,8 +7,15 @@ HW08 -- Refactor Freshie Zero
 
 /**
 DISCOVERIES
-
+The main method can be used to test the other methods by invoking and
+displaying the results.
+System.out.println seems to display the results as dots of light rather
+than returning the outputs.
+The class name must be the same as the file name.
 UNRESOLVED QUESTIONS
+What does public do in a function, how would the function run if it were
+private?
+What does static do in java functions and what are the alteritives?
 */
 public class Greet {
 	public static void main(String[] args) {
