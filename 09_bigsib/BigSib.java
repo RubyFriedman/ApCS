58032bb6f331359445b91fb0d46a007f1add96ae
rubyfriedman <rfriedman30@stuy.edu>
2021-10-03 21:39:21 -0400

    init setup

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,7 @@
+public class BigSib {
+	public static String greet(String x) {
+                String beg = "Why, hello there, ";
+                String end = ". How do you do?";
+                return beg + x + end;
+	}
+}
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,7 @@
+public class Greet {
+	public static void main( String[] args ) {
+        System.out.println(BigSib.greet("Flim"));
+        System.out.println(BigSib.greet("Flam"));
+        System.out.println(BigSib.greet("Kazaam"));
+	}
+}
