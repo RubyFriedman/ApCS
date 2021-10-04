07ec16c41d2dcf1988608eaf6abc463894135b67
rubyfriedman <rfriedman30@stuy.edu>
2021-10-03 21:51:48 -0400

    init setup

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,3 +1,16 @@
+/**
+Team JAR (Ruby Friedman and their ducky Tiffany, Ariel Fuchs and his ducky Skelly duckler III, Joshua Gao  and his ducky Batman)
+APCS
+HW09 -- What Are BigSibs Good For?
+2021-10-03
+*/
+
+/**
+DISCOVERIES
+
+UNRESOLVED QUESTIONS
+*/
+
 public class BigSib {
 	public static String greet(String x) {
                 String beg = "Why, hello there, ";
