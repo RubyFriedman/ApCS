d4133d92bb7fa69d57b7fbd86a15aa6c74763403
rubyfriedman <rfriedman30@stuy.edu>
2021-10-25 21:34:55 -0400

    init setup

---
+++ 24_while/Coin.java
@@ -0,0 +1,170 @@
+
+import java.lang.Math;
+public class Coin {
+
+	//attributes aka instance vars
+	private double value;
+	private String upFace, name;
+	private int flipCtr, headsCtr, tailsCtr;
+	private double bias = 0.5;
+
+	/***
+	*  Coin() -- default constuctor
+	*  precond:
+	*  postcond:
+	***/
+	public Coin() {
+		reset( "heads", 0.5 );
+	}
+
+
+	/***
+	Coin(String) -- overloaded constructor
+	precond: input is one of
+	"penny",
+	"nickel",
+	"dime",
+	"quarter",
+	"half dollar",
+	"dollar"
+	postcond:
+	***/
+	public Coin( String s ) {
+		this();
+		name = s;
+		assignValue(s);
+		}
+
+
+	/***
+	Coin(String,String) --
+	precond:
+	postcond:
+	***/
+	public Coin( String s, String nowFace ) {
+		this(s);
+		upFace = nowFace;
+	}
+
+
+	// Accessors...
+	// ----------------------------
+	public String getUpFace() {
+		return upFace;
+	}
+
+	public int getFlipCtr() {
+		return flipCtr;
+	}
+
+	public double getValue() {
+        	return value;
+	}
+
+	public int getHeadsCtr() {
+        	return headsCtr;
+	}
+
+	public int getTailsCtr() {
+		return tailsCtr;
+
+	}
+	// ----------------------------
+
+
+	/***
+	* assignValue() -- set a Coin's monetary value based on its name
+	* precond:  input String is a valid coin name ("penny", "nickel", etc.)
+	* postcond: instvar value gets appropriate value
+	* Returns value assigned.
+	***/
+	private double assignValue( String s ) {
+		if (s == "penny") {
+                        value = 0.01;
+                }
+                else if (s == "nickel") {
+                        value = 0.05;
+                }
+                else if (s == "dime") {
+                        value = 0.10;
+                }
+                else if (s == "quarter") {
+                        value = 0.25;
+                }
+                else if (s == "half dollar") {
+                        value = 0.50;
+                }
+                else if (s == "dollar") {
+                        value = 1.00;
+                }
+		return value;
+	}
+
+
+	/***
+	reset() -- initialize a Coin
+	precond:  s is "heads" or "tails", 0.0 <= d <= 1.0
+	postcond: Coin's attribs reset to starting vals
+	***/
+	public void reset( String s, double d ) {
+                upFace = s;
+                flipCtr = 0;
+                headsCtr = 0;
+                tailsCtr = 0;
+                bias = d;
+	}
+
+
+	/***
+	* String flip() -- simulates a Coin flip
+	* precond:  bias is a double on interval [0.0,1.0]
+	* (1.0 indicates always heads, 0.0 always tails)
+	* postcond: upFace updated to reflect result of flip.
+	* flipCtr incremented by 1.
+	* Either headsCtr or tailsCtr incremented by 1, as approp.
+	* Returns "heads" or "tails"
+	***/
+
+	public String flip() {
+		double randDbl = Math.random();
+		if (randDbl < bias) {
+			upFace = "heads";
+			headsCtr += 1;
+		}
+		else {
+			upFace = "tails";
+                        tailsCtr += 1;
+		}
+		flipCtr += 1;
+		return upFace;
+	}
+
+
+	/***
+	* boolean equals(Coin) -- checks to see if 2 coins have same face up
+	* precond: other is not null
+	* postcond: Returns true if both coins showing heads
+	* or both showing tails. False otherwise.
+	***/
+	public boolean equals( Coin other ) {
+		if (other != null) {
+			return upFace.equals(other.upFace);
+		}
+		else {
+			return false;
+		}
+	}
+
+	/***
+	* String toString() -- overrides toString() provided by Java
+	* precond: n/a
+	* postcond: Return String comprised of name and current face
+	***/
+	public String toString() {
+		String retStr = "";
+		retStr = retStr + name;
+		retStr = retStr + " -- " + upFace;
+		return retStr;
+	}
+
+}//end class
---
+++ 24_while/Driver.java
@@ -0,0 +1,68 @@
+/**
+Team Watson + Tiffany (Ruby Friedman & Anjini Katari)
+APCS
+HW24 -- Get It While You Can
+2021-10-26
+time spent:
+DISCO:
+ * this() in the 2nd constructor asks java to rerun the 2st constructor
+   (which is assigned to this()), give the input value of s, and assigns
+   upFace to nowFace
+ * You can nest a conditional in a while loop
+QCC:
+ * Still a bit confused on how to use this()
+ * Why does private/public matter/effect the outcomes? 
+POST-v0 MODS:
+ * Changed public vars to private 
+ * Added this() and .equals() to make code more efficient
+ * Initialized multiple vars in 1 line -> to save space/condense code
+**/
+
+public class Driver {
+	public static void main(String[] args) {
+	//x heads have come up
+		Coin Watson = new Coin("penny", "tails");
+		int x = 75;
+		int y = 123;
+		int counter0 = 0;
+		int matchNum = 0;
+		int counter2 = 0;		
+
+		while (Watson.getHeadsCtr() < x) {
+			Watson.flip();
+			counter0 += 1;
+		}
+		
+		System.out.println("It took " + counter0 + " flips to get " + x + " heads." );
+	//y matches have occured
+		Coin tiffany = new Coin("quarter", "heads");
+		while (matchNum < y) {
+			Watson.flip();
+			tiffany.flip();
+			if (tiffany.getUpFace() == Watson.getUpFace()) {
+				matchNum += 1;
+			}
+			counter2 += 1;
+		}
+		System.out.println("It took " + counter2 + " flips to get " + y + " matches." );
+
+	//at least 65536 matches have come up, and the number of matches is divisible by the year of your birth.
+		
+		int matchNumber = 0;
+		int counter3 = 0;	
+
+		Coin Wuby = new Coin("dime", "tails");
+		Coin Diny = new Coin("dollar", "heads");
+		while (matchNumber < (2005 * 65536)) { 
+			Wuby.flip();
+			Diny.flip();
+			if (Wuby.getUpFace() == Diny.getUpFace()) {
+                                matchNumber += 1;
+                        }
+                        counter3 += 1;
+		}
+                System.out.println("It took " + counter3 + " flips to get at least 65536 matches have come up, and the number of matches is divisible by the year of your birth." );
+
+	}//end main()
+
+}//end class
