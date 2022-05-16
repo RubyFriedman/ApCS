b78191b0f8e808a5ba50ef0831768864c444b182
rubyfriedman <rfriedman30@stuy.edu>
2022-05-15 22:08:14 -0400

    yas.

--- 99_alpq/ArrayPriorityQueue.java
+++ 99_alpq/ArrayPriorityQueue.java
@@ -1,3 +1,10 @@
+/**
+Jerk Kafe :: Joseph, Ruby, Kosta
+APCS pd7
+HW99 -- Some Are More Equal Than Others, Codified
+2022-05-16m
+time spent: 1.0 hrs
+*/
 import java.util.ArrayList;
 
 public class ArrayPriorityQueue implements PriorityQueue {
