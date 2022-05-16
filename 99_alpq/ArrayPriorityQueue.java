897313b0881fdc7eaa97eb905b81c65d0cd58818
rubyfriedman <rfriedman30@stuy.edu>
2022-05-15 20:12:56 -0400

    not done

---
+++ 99_alpq/ArrayPriorityQueue.java
@@ -0,0 +1,65 @@
+import Java.util.ArrayList;
+
+public class ArrayPriorityQueue {
+  /**
+    ArrayPriorityQueue class
+      IVs
+        - ArrayList<>() al
+        +
+      Methods
+        -
+        +
+  */
+
+  private ArrayList<Integer> al;
+  public int size;
+
+  public ArrayPriorityQueue() {
+    al = new ArrayList<Integer>();
+    size = 0;
+  }
+
+  public void add(int x) {
+    int ind = al.size() - 1;
+    while (ind >= 0 && al.get(ind) < x) {
+      ind--;
+    }
+    al.add(ind, x);
+    size++;
+  }
+
+  public boolean isEmpty() {
+    return al.size() == 0;
+  }
+
+  public int peekMin() {
+    return al.get(al.size() - 1);
+  }
+
+  public int removeMin() {
+    size--;
+    return al.remove(al.size() - 1);
+  }
+
+  public int size() {
+    return al.size();
+  }
+
+  public String toString() {
+    String retVal = "";
+    for(int i = size-1; i>=0; i--) {
+      
+    }
+  }
+
+  public static void main(String[] args) {
+    ArrayPriorityQueue KDubs = new ArrayPriorityQueue();
+    KDubs.add(4);
+    KDubs.add(4);
+    KDubs.add(4);
+    KDubs.add(4);
+    KDubs.add(4);
+    KDubs.add(4);
+    KDubs.add(4);
+  }
+}
